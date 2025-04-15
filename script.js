7859d29ad3f284f0bc7373b5e0a7c5aa56f0818b
Reb1azzze <82503613+Reb1azzze@users.noreply.github.com>
2025-04-15 15:00:04 +0300

    same

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { MatchThree } from "./match-three.js";
 var WebApp = window.Telegram.WebApp;
 
-WebApp.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
+WebApp.sendData("wioerhioehrgoiherg");
 
 let duration = 12;
 let gameInstance;
